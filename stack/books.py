aff4aae296dba3485b10b60fa4e4003d9fbd2658
Vitaly Astra <va.lisenko@alregn.ru>
2025-12-26 14:38:24 +0700

    fix

--- stack/books.py
+++ stack/books.py
@@ -1,6 +1,12 @@
 def main():
-    n = int(input())
+    input()
     books = list(map(int, input().split()))
+    max_length = 0
+    balanced_segment = []
+
+    for book in books:
+        if book % 2 == 1:
+            pass
 
 
 if __name__ == "__main__":
