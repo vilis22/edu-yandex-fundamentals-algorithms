baf82adbcf0287492d2d8913ab6bd13336358a02
Vitaly Astra <va.lisenko@alregn.ru>
2026-09-07 14:53:44 +0700

    fix

---
+++ oop/Python_OOP/141.py
@@ -0,0 +1,7 @@
+class Acne:
+    def __init__(self):
+        self.status = "активный"
+
+    def explosion(self):
+        print("Бегите, глупцы!")
+        self.status = "уничтожен"
---
+++ oop/Python_OOP/142.py
@@ -0,0 +1,12 @@
+class Singer:
+    def __init__(self, name="Анна"):
+        self.name = name
+
+    def voice_1(self):
+        print("Звуки сморкающегося хомячка")
+
+    def voice_2(self):
+        print("Кашляющее в форточку кенгуру")
+
+    def voice_3(self):
+        print("Поперхнувшаяся квасом креветка")
